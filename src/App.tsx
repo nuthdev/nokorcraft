83bfaab08d318f4e93b2826b4cd8ec1829beaab9
nuthdev <nuthmc7@gmail.com>
2025-10-04 07:46:30 +0700

    dvelopment

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
 import {AppContext} from "./context/AppContext.tsx";
 import AppRouter from "./router/AppRouter.tsx";
-
+import {useEffect} from "react";
+import './App.css'
 const App = () => {
+    useEffect(() => {
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                console.log(entry.target);
+                if (entry.isIntersecting) {
+                    entry.target.classList.add('fade-up');
+
+                    observer.unobserve(entry.target)
+                }
+            })
+        })
+        const elements = document.querySelectorAll('.box')
+        elements.forEach(ep => observer.observe(ep))
+    })
     console.log(window.matchMedia("(prefers-color-scheme: dark)").matches);
     if (localStorage.getItem('darkMode') == 'true') {
         document.documentElement.classList.add('dark');
     } else {
         document.documentElement.classList.remove('dark');
     }
+
     return (
         <>
             <AppContext>
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,14 +3,15 @@ import {Link} from "react-router-dom";
 const Home = () => {
 
     return (
-        <div className='container flex flex-col mt-5 text-center text-base text-black place-items-center dark:text-white'>
+        <div className='flex flex-col mt-5 text-center text-base text-black place-items-center dark:text-white'>
             <h1 className='font-[Minecraft] text-3xl'>Nokorcraft</h1>
             <p className=''>NokorCraft is a minecraft server network in cambodia aiming to provide a wide variety of game modes and an extensive community</p>
-            <section className='grid grid-cols-2 gap-2 w-[60%] max-w-3/6 box-border mt-7 *:h-10 *:rounded *:w-full *:flex *:justify-center *:items-center *:hover:opacity-65 lg:grid-cols-3 text-white'>
-                <Link to='/' className=' bg-red-600'>Vote</Link>
-                <Link to='/' className='bg-blue-600'>Rule</Link>
-                <Link to='/' className='bg-green-600 col-span-2 lg:col-span-1'>Become a content creator</Link>
+            <section className='grid grid-cols-2 gap-2 w-[90%]  box-border mt-7 *:h-10 *:rounded *:w-full *:flex *:justify-center *:items-center lg:grid-cols-3 text-white'>
+                <Link to='/' className='bg-red-600 box hover:opacity-80 fade-up'>Vote</Link>
+                <Link to='/' className='bg-blue-600 box'>Rule</Link>
+                <Link to='/' className='bg-green-600 col-span-2 lg:col-span-1 box'>Become a content creator</Link>
             </section>
+            <div className='w-[100vw] h-[200px] bg-cover bg-center bg-fixed lg:order-[-1] mt-4 relative after:content-[""] after:absolute after:left-0 after:top-0 after:w-full after:h-full after:bg-neutral-900/50' style={{backgroundImage: 'url(/banner.webp)'}}></div>
         </div>
     )
 }
